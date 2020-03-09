c867a07b644edf6da9f43f877c4c2b6b591b8f29
Angelina9980 <61983833+Angelina9980@users.noreply.github.com>
2020-03-10 01:45:03 +0400

    Add files via upload

---
+++ Task5.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the sentence with '()', '[]' and '{}'");
+            string sentence = Console.ReadLine();
+
+            Console.WriteLine(Check(sentence));
+              Console.ReadKey();
+                                   
+        }
+
+       public static bool Check(string sentence)
+        {
+            int[] array = new int[sentence.Length];
+            int mark = 0;
+
+            if (sentence[0] == ')' || sentence[0] == ']' || sentence[0] == '}')
+                return false;
+
+            for (int i = 0; i < sentence.Length;i++)
+            {
+                if (sentence[i] == '(')
+                {
+                    array[mark] = 1;
+                    mark++;
+                }
+
+                if (sentence[i] == '[')
+                {
+                    array[mark] = 2;
+                    mark++;
+                }
+
+                if (sentence[i] == '{')
+                {
+                    array[mark] = 3;
+                    mark++;
+                }
+
+
+                if (sentence[i] == ')')
+                {
+                    if (mark == 0 || array[mark - 1] == 2 || array[mark - 1] == 3)
+                        return false;
+                    else mark--;
+                }
+
+                if (sentence[i] == ']')
+                {
+                    if (mark == 0 || array[mark - 1] == 1 || array[mark - 1] == 3)
+                        return false;
+                    else mark--;
+                }
+
+                if (sentence[i] == '}')
+                {
+                    if (mark == 0 || array[mark - 1] == 1 || array[mark - 1] == 2)
+                        return false;
+                    else mark--;
+                }
+                
+            }
+                if (mark != 0) return false;
+                else return true;
+        }
+    }
+}
